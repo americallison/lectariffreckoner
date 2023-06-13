31f77ed9f92d6493e388577e953d2cbaf85e6e56
Americ Allison <96029332+americallison@users.noreply.github.com>
2023-06-13 11:34:38 +0000

    switch positions of fixed charge

--- src/components/AmounttoConsumption.js
+++ src/components/AmounttoConsumption.js
@@ -13,46 +13,45 @@ export default function AmounttoConsumption({ vendingDate, setVendingDate, total
       <form>
 
         <p className='p-1'><label>Last Month Vended: </label>
-          <input className="w-50 font-medium p-1 border" value={vendingDate} name="vendingdate" type='date' 
+          <input className="w-50 font-medium p-1 border" value={vendingDate} name="vendingdate" type='date'
             onChange={(e) => setVendingDate(e.target.value)} required />
         </p>
 
-       <hr />
+        <hr />
 
-        
-          <p className='p-1'><label>Total Amount (USD): </label>
-            <input className="p-1 bg-sky-100 border-b-4 border-yellow-300 shadow-sm leading-tight 
-        focus:outline-none mb-1 text-gray-700 font-light w-full md:w-12/12 bg-white"
-              id='amount' name='totalamount' value={totalAmount}
-              onChange={(e) => setTotalAmount(e.target.value)} placeholder='Enter amount' type="number" required /></p>
 
-          <p className="p-1"><label>Energy Charge (USD): </label>
-            <input className="p-1 bg-stone-200 border-b-4 border-yellow-300 shadow-sm leading-tight 
+        <p className='p-1'><label>Total Amount (USD): </label>
+          <input className="p-1 bg-sky-100 border-b-4 border-yellow-300 shadow-sm leading-tight 
         focus:outline-none mb-1 text-gray-700 font-light w-full md:w-12/12 bg-white"
-              id='energy_charge' value={EnergyCharge} name='energycharge' type="number"
-            /></p>
+            id='amount' name='totalamount' value={totalAmount}
+            onChange={(e) => setTotalAmount(e.target.value)}
+            min="0" placeholder='Enter amount' type="number" required /></p>
 
-       
-      
-          <p className='p-2'><label>GST (10% of Energy Charge): </label>
-            <input className="p-2 bg-stone-200 border-b-4 border-yellow-300 shadow-sm leading-tight 
+        <p className="p-1"><label>Energy Charge (USD): </label>
+          <input className="p-1 bg-stone-200 border-b-4 border-yellow-300 shadow-sm leading-tight 
         focus:outline-none mb-1 text-gray-700 font-light w-full md:w-12/12 bg-white"
-              id='gst_energy_charge' name='gstenergycharge' type="text" value={GSTEnergyCharge} /></p>
+            id='energy_charge' value={EnergyCharge} name='energycharge' type="number"
+          /></p>
 
-          <p className='p-2'><label>Fixed Charge (USD/month): </label>
-            <input className="p-2 bg-stone-200 border-b-4 border-yellow-300 shadow-sm leading-tight 
+        <p className='p-2'><label>Fixed Charge (USD/month): </label>
+          <input className="p-2 bg-stone-200 border-b-4 border-yellow-300 shadow-sm leading-tight 
         focus:outline-none mb-1 text-gray-700 font-light w-full md:w-12/12 bg-white"
-              id='fixed_charge' name='fixedcharge' type="text" value={FixedChargePerMonth} /></p>
-        
+            id='fixed_charge' name='fixedcharge' type="text" value={FixedChargePerMonth} /></p>
+
+        <p className='p-2'><label>GST (10% of Energy Charge): </label>
+          <input className="p-2 bg-stone-200 border-b-4 border-yellow-300 shadow-sm leading-tight 
+        focus:outline-none mb-1 text-gray-700 font-light w-full md:w-12/12 bg-white"
+            id='gst_energy_charge' name='gstenergycharge' type="text" value={GSTEnergyCharge} /></p>
+
         <div>
           <p className='p-1'><label>Consumption (Kwh): </label>
             <input className="p-1 bg-stone-200 border-b-8 font-bold border-black shadow-sm leading-tight 
         focus:outline-none mb-2 w-full md:w-12/12 text-xl"
               id='consumption_kwh' name='consumptionkwh' type="number" value={ConsumptionKwh} /></p>
         </div>
-        <p className="m-1 p-2 text-xl font-light bg-yellow-50">If you recharge&nbsp; 
-        <strong><em>{totalAmount}USD</em></strong>,
-         you will get <strong><em>{ConsumptionKwh}Kwh.</em></strong> </p>
+        <p className="m-1 p-2 text-xl font-light bg-yellow-50">If you recharge&nbsp;
+          <strong><em>{totalAmount}USD</em></strong>,
+          you will get <strong><em>{ConsumptionKwh}Kwh.</em></strong> </p>
       </form>
     </div>
 
--- src/components/ConsumptiontoAmount.js
+++ src/components/ConsumptiontoAmount.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 export default function ConsumptiontoAmount({ vendingDate, setVendingDate,
-     EnergyCharge, GSTEnergyCharge, FixedChargePerMonth,
+    EnergyCharge, GSTEnergyCharge, FixedChargePerMonth,
     ConsumptionKwhFirst, setConsumptionKwhFirst, totalAmountLast }) {
 
     return (
@@ -13,41 +13,40 @@ export default function ConsumptiontoAmount({ vendingDate, setVendingDate,
                     <input className="w-50 font-medium p-2 border" value={vendingDate} name="vendingdate" type='date' required
                         onChange={(e) => setVendingDate(e.target.value)} />
                 </p>
-            
 
-               <hr />
-                
-                    <p className='p-2'><label>Consumption (Kwh): </label>
-                        <input className="p-1 bg-stone-100 border-b-4 border-yellow-300 shadow-sm leading-tight 
+
+                <hr />
+
+                <p className='p-2'><label>Consumption (Kwh): </label>
+                    <input className="p-1 bg-stone-100 border-b-4 border-yellow-300 shadow-sm leading-tight 
         focus:outline-none mb-3 text-gray-700 font-light w-full md:w-12/12 bg-white"
-                            id='consumption_kwh' name='consumptionkwh' type="text" value={ConsumptionKwhFirst}
-                            placeholder='Enter consumption (Kwh)' onChange={(e) => setConsumptionKwhFirst(e.target.value)} /></p>
+                        id='consumption_kwh' name='consumptionkwh' type="text" value={ConsumptionKwhFirst}
+                        placeholder='Enter consumption (Kwh)' onChange={(e) => setConsumptionKwhFirst(e.target.value)} /></p>
 
 
-                    <p className="p-2"><label>Energy Charge (USD): </label>
-                        <input className="p-1 bg-stone-100 border-b-4 border-yellow-300 shadow-sm leading-tight 
+                <p className="p-2"><label>Energy Charge (USD): </label>
+                    <input className="p-1 bg-stone-100 border-b-4 border-yellow-300 shadow-sm leading-tight 
         focus:outline-none mb-3 text-gray-700 font-light w-full md:w-12/12 bg-white"
-                            id='energy_charge' value={EnergyCharge} name='energycharge' type="number"
-                        /></p>
+                        id='energy_charge' value={EnergyCharge} name='energycharge' type="number"
+                    /></p>
 
-              
-               
-                    <p className='p-2'><label>GST (10% of Energy Charge): </label>
-                        <input className="p-1 bg-stone-100 border-b-4 border-yellow-300 shadow-sm leading-tight 
+                <p className='p-2'><label>Fixed Charge (USD/month): </label>
+                    <input className="p-1 bg-stone-100 border-b-4 border-yellow-300 shadow-sm leading-tight 
         focus:outline-none mb-3 text-gray-700 font-light w-full md:w-12/12 bg-white"
-                            id='gst_energy_charge' name='gstenergycharge' type="text" value={GSTEnergyCharge} /></p>
+                        id='fixed_charge' name='fixedcharge' type="text" value={FixedChargePerMonth} /></p>
 
-                    <p className='p-2'><label>Fixed Charge (USD/month): </label>
-                        <input className="p-1 bg-stone-100 border-b-4 border-yellow-300 shadow-sm leading-tight 
+                <p className='p-2'><label>GST (10% of Energy Charge): </label>
+                    <input className="p-1 bg-stone-100 border-b-4 border-yellow-300 shadow-sm leading-tight 
         focus:outline-none mb-3 text-gray-700 font-light w-full md:w-12/12 bg-white"
-                            id='fixed_charge' name='fixedcharge' type="text" value={FixedChargePerMonth} /></p>
-              
+                        id='gst_energy_charge' name='gstenergycharge' type="text" value={GSTEnergyCharge} /></p>
+
+
                 <div>
                     <p className='p-2'><label>Total Amount (USD): </label>
                         <input className="p-1 bg-sky-100 border-b-4 border-black shadow-sm leading-tight 
 focus:outline-none mb-3 text-gray-700 font-light w-full md:w-12/12 bg-white"
                             id='amount' name='totalamount' value={totalAmountLast}
-                           type="number" required /></p>
+                            type="number" required /></p>
 
                 </div>
             </form>
