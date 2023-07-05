2820124d2667ff71f71b4b8d340e18f700f9a0e6
Americ Allison <96029332+americallison@users.noreply.github.com>
2023-07-05 15:33:32 +0000

    energy

--- src/components/AmounttoConsumption.js
+++ src/components/AmounttoConsumption.js
@@ -2,108 +2,124 @@ import React, { useState, useEffect } from 'react';
 
 
 export default function AmounttoConsumption({ vendingMonth, setVendingMonth, vendingYear, setVendingYear,
-   totalAmount, setTotalAmount, EnergyChargeSocial, EnergyCharge, GSTEnergyCharge, GSTEnergyChargeSocial,
-    FixedChargePerMonth,ConsumptionKwh, socialConsumptionKwh, consumerType, negtotal, 
-    negconsumption, months, years
-}) 
-  
-  {
-    return (
-      <>
-         <form>
-         <div className="flex mb-4">
-        <div className="w-1/2 mr-2">
-          <label htmlFor="lastVendingMonth" className="block font-medium mb-1">
-            Last Vending Month
-          </label>
-          <select
-            id="vendingMonth"
-            value={vendingMonth}
-            onChange={(e) => setVendingMonth(e.target.value)}
-            className="w-full border-none bg-slate-200 rounded-md px-3 py-2 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-          >
-            
+  totalAmount, setTotalAmount, EnergyChargeSocial, EnergyCharge, GSTEnergyCharge, GSTEnergyChargeSocial,
+  FixedChargePerMonth, ConsumptionKwh, socialConsumptionKwh, consumerType, negtotal,
+  negconsumption, months, years
+}) {
+  return (
+    <>
+      <form>
+        <div className="flex mb-4">
+          <div className="w-1/2 mr-2">
+            <label htmlFor="lastVendingMonth" className="block font-medium mb-1">
+              Last Vending Month
+            </label>
+            <select
+              id="vendingMonth"
+              value={vendingMonth}
+              onChange={(e) => setVendingMonth(e.target.value)}
+              className="w-full border-none bg-slate-200 rounded-md px-3 py-2 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+            >
+
               {months.map((month) => (
                 <option key={month.id} value={month.label}>{month.label}</option>
-            ))}
-        
-          </select>
-        </div>
-        <div className="w-1/2 ml-2">
-          <label htmlFor="lastVendingYear" className="block font-medium mb-1">
-            Last Vending Year
-          </label>
-          <select
-            type="number"
-            id="vendingYear"
-            value={vendingYear}
-            onChange={(e) => setVendingYear(e.target.value)}
-            className="w-full border-none bg-slate-200 rounded-md px-3 py-2 focus:outline-none focus:ring-blue-500 focus:border-black-500"
-          >
-            {years.map((year) => (
+              ))}
+
+            </select>
+          </div>
+          <div className="w-1/2 ml-2">
+            <label htmlFor="lastVendingYear" className="block font-medium mb-1">
+              Last Vending Year
+            </label>
+            <select
+              type="number"
+              id="vendingYear"
+              value={vendingYear}
+              onChange={(e) => setVendingYear(e.target.value)}
+              className="w-full border-none bg-slate-200 rounded-md px-3 py-2 focus:outline-none focus:ring-blue-500 focus:border-black-500"
+            >
+              {years.map((year) => (
                 <option key={year.id} value={year.label}>{year.label}</option>
-            ))}
-        
-          </select>
+              ))}
+
+            </select>
+          </div>
         </div>
-      </div>
 
-      <div className='flex justify-between'>
-                            <div className='w-4/12'>
-                    <label className='p-2'>Total Amount:</label>
-                    </div>
-                    <div className="w-8/12">
-                        <input className="bg-slate-200 p-2 border-none rounded shadow-sm leading-tight 
+        <div className='flex justify-between'>
+          <div className='w-4/12'>
+            <label className='p-2'>Total Amount:</label>
+          </div>
+          <div className="w-8/12">
+            <input className="bg-slate-200 p-2 border-none rounded shadow-sm leading-tight 
         focus:outline-none mb-2 text-gray-700 font-light w-full" type="number" id={totalAmount}
-                            value={totalAmount} onChange={(e) => setTotalAmount(e.target.value)}
-                           
-                        />
-                        </div></div>
+              value={totalAmount} onChange={(e) => setTotalAmount(e.target.value)}
 
-                        {
-                          consumerType === "Residential" ? 
-                          (<>
-                             <div className="flex justify-between">
-                              <div className="w-4/12"></div>
-                              <div className="w-4/12">
-                                <p className='font-medium m-2'>Social</p>
-                              </div>
-                              <div className="w-4/12">
-                                <p className='font-medium m-2'>Residential</p>
-                              </div>
-                             </div>
-                          </>) : (<div>
+            />
+          </div></div>
 
-                          </div>)
-                        }
-                
-                        <div className='flex justify-between'>
-                            <div className='w-4/12'>
-                    <label className='p-2'>Energy Charge:</label>
-                    </div>
-                    <div className="w-8/12">
-                        <input className="bg-red-200 p-2 border-none rounded shadow-sm leading-tight 
+        {
+          consumerType === "Residential" ?
+            (<>
+              <div className="flex justify-between">
+                <div className="w-4/12"></div>
+                <div className="w-4/12">
+                  <p className='font-medium m-2'>Social</p>
+                </div>
+                <div className="w-4/12">
+                  <p className='font-medium m-2'>Residential</p>
+                </div>
+              </div>
+
+              <div className="flex justify-between">
+                <div className="w-4/12">
+                  <label className='p-2'>Energy Charge</label>
+                </div>
+                <div className="w-4/12">
+                <input className="bg-red-200 p-2 border-none rounded shadow-sm leading-tight 
         focus:outline-none mb-2 text-gray-700 font-light w-full" type="number" id="energyCharge"
-                            value={totalAmount} onChange={(e) => setTotalAmount(e.target.value)}
-                           
-                        />
-                        </div></div>
+              value={EnergyChargeSocial}
+            />
+                </div>
+                <div className="w-4/12">
+                <input className="bg-red-200 p-2 border-none rounded shadow-sm leading-tight 
+        focus:outline-none mb-2 text-gray-700 font-light w-full" type="number" id="energyCharge"
+              value={EnergyCharge} 
+            />
+                </div>
+              </div>
+            </>) : (<div>
+
+            </div>)
+        }
+
+        <div className='flex justify-between'>
+          <div className='w-4/12'>
+            <label className='p-2'>Energy Charge:</label>
+          </div>
+          <div className="w-8/12">
+            <input className="bg-red-200 p-2 border-none rounded shadow-sm leading-tight 
+        focus:outline-none mb-2 text-gray-700 font-light w-full" type="number" id="energyCharge"
+              value={totalAmount} onChange={(e) => setTotalAmount(e.target.value)}
+
+            />
+          </div></div>
 
 
 
-      <div className="mb-4">
-        <label htmlFor="gstEnergyCharge" className="block font-medium mb-1">
-          GST Energy Charge
-        </label>
-        <input
-          type="number"
-          id="gstEnergyCharge"
-          value={GSTEnergyCharge}
-          className="w-full border-gray-300 border rounded-md px-3 py-2 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-        />
-      </div>
+        <div className="mb-4">
+          <label htmlFor="gstEnergyCharge" className="block font-medium mb-1">
+            GST Energy Charge
+          </label>
+          <input
+            type="number"
+            id="gstEnergyCharge"
+            value={GSTEnergyCharge}
+            className="w-full border-gray-300 border rounded-md px-3 py-2 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+          />
+        </div>
 
-         </form>
-      </>
-    )
-  }
+      </form>
+    </>
+  )
+}
